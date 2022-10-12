9f864803d93d75ad551c7edde54fbab225d7b70c
MateuszZaniewski <mateusz.zaniewski94@gmail.com>
2022-10-12 08:43:39 +0200

    addes scss version and mobile first

---
+++ js/main.js
@@ -0,0 +1,13 @@
+const hamburgerMenu = document.querySelector('.hamburger--Menu')
+hamburgerMenu.addEventListener('click', showMenu)
+
+const logoContainer = document.querySelector('.logo_container')
+
+const menuMobile = document.querySelector('.menu--mobile')
+
+
+function showMenu() {
+    hamburgerMenu.classList.toggle('open')
+    menuMobile.classList.toggle('hidden')
+    
+}
