b19568e57ad092eaaf7ad3f84ce451d8602978bf
MateuszZaniewski <mateusz.zaniewski94@gmail.com>
2022-10-15 18:19:27 +0200

    new section added and fixed bugs in previous sections

--- js/main.js
+++ js/main.js
@@ -13,3 +13,31 @@ function showMenu() {
     mainContent.classList.toggle('hidden')
     
 }
+
+
+const firstMenuRoll = document.querySelector('.firstOfFour')
+firstMenuRoll.addEventListener('click', showHiddenList1)
+const content1 = document.querySelector('.content1')
+const content2 = document.querySelector('.content2')
+
+function showHiddenList1 () {
+    content1.classList.toggle('hidden')
+    console.log('Uncover')
+}
+
+const secondMenuRoll = document.querySelector('.secondOfFour')
+secondMenuRoll.addEventListener('click', showHiddenList2)
+
+function showHiddenList2 () {
+    content2.classList.toggle('hidden')
+    console.log('Uncover')
+}
+
+const proposalInfo = document.querySelector('.proposal--info')
+
+proposalArray = document.querySelectorAll('.proposalList--option')
+proposalArray.forEach((el) => {
+    el.addEventListener('click', () => {
+        el.childNodes[3].classList.toggle('hidden')
+    })
+})
