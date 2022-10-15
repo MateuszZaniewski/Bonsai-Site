fb44a4c29f2ea17ce2ebd1b3926dcd3169547a34
MateuszZaniewski <mateusz.zaniewski94@gmail.com>
2022-10-15 13:58:15 +0200

    navbar bug fixes with overlay

--- js/main.js
+++ js/main.js
@@ -1,6 +1,8 @@
 const hamburgerMenu = document.querySelector('.hamburger--Menu')
 hamburgerMenu.addEventListener('click', showMenu)
 
+const mainContent = document.querySelector('.mainContent')
+
 const logoContainer = document.querySelector('.logo_container')
 
 const menuMobile = document.querySelector('.menu--mobile')
@@ -8,5 +10,6 @@ const menuMobile = document.querySelector('.menu--mobile')
 function showMenu() {
     hamburgerMenu.classList.toggle('open')
     menuMobile.classList.toggle('hidden')
+    mainContent.classList.toggle('hidden')
     
 }
